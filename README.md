3909bd2e1595eb4c59692425b1f16c5133246273
Louis Pilfold <louis@lpil.uk>
2016-05-01 22:06:08 +0100

    Document usage

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Stepper
 =======
 
 A little JS step sequencer.
+
+```sh
+# Run the test runner
+npm test
+
+# Run the dev server
+npm start
+```
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: "babel",
         query: {
           presets: ["es2015"],
           plugins: ["transform-object-assign"],
