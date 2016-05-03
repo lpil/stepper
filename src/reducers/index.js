c4a6ebcb6119d007b7dbc0995d88bd61a8ebb22f
Louis Pilfold <louis@lpil.uk>
2016-05-03 21:07:41 +0100

    Add reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
-import bpm  from "./bpm";
+import bpm    from "./bpm";
+import cells  from "./cells";
+import loaded from "./loaded";
 
 const rootReducer = combineReducers({
   bpm,
+  cells,
+  loaded,
 });
 
 export default rootReducer;
