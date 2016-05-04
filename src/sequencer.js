d236532f73acbdfed8d6bc714f0fc80cbf5ca132
Louis Pilfold <louis@lpil.uk>
2016-05-04 20:18:49 +0100

    Instruments can be triggered based upon state

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,13 +1,22 @@
 import { applyEvery } from "./scheduler";
+import { member } from "./structures/cell_set";
 
 const steps = 16;
+const rows  = 16;
 const bpm   = 120;
 
+function play(step, inst) {
+  console.log("inst", inst, "triggered");
+}
+
 function playStep(step, store) {
-  const cellSet  = store.getState().cells;
-  const nextStep = (step + 1) % steps;
-  console.log("playing step ", step, "with cells ", cellSet);
-  return [nextStep, store];
+  const cells = store.getState().cells;
+  const next  = (step + 1) % steps;
+  let y = rows;
+  while (y--) {
+    if (member(cells, step, y)) { play(step, y); }
+  }
+  return [next, store];
 }
 
 function init(store, ctx) {
